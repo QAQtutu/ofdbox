f941fab97f89d9237db6598e4ea50ae365c9c9fe
Donal <donal.tong@gmail.com>
2020-10-27 11:50:10 +0800

    测试路径

--- ordbox-converter/src/test/java/com/qaqtutu/ofdbox/pdf/PdfConverterTest.java
+++ ordbox-converter/src/test/java/com/qaqtutu/ofdbox/pdf/PdfConverterTest.java
@@ -9,28 +9,23 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class PdfConverterTest {
-    private static File base=new File("target/test/pdf");
-
+    protected static final String basePath = Objects.requireNonNull(PdfConverterTest.class.getClassLoader().getResource("")).getPath();
     @Test
     public void init() throws IOException {
-        if(!base.exists()){
-            FileUtils.forceMkdir(base);
-        }
     }
 
     @Test
     public void toPdf() throws IOException {
 
         OFDReader reader = new OFDReader();
-        OFD ofd = reader.read(new File("src/test/resources/发票.ofd"));
-        OFD ofd1 = reader.read(new File("src/test/resources/旋转测试.ofd"));
-
+        OFD ofd = reader.read(new File(basePath+"发票.ofd"));
+        OFD ofd1 = reader.read(new File(basePath+"旋转测试.ofd"));
 
         Ofd2pdf ofd2pdf = new Ofd2pdf();
-        ofd2pdf.toPdf(ofd.getDocuments().get(0), new File(base,"发票.pdf"));
-        ofd2pdf.toPdf(ofd1.getDocuments().get(0), new File(base,"旋转测试.pdf"));
-
+        ofd2pdf.toPdf(ofd.getDocuments().get(0), new File(basePath,"发票.pdf"));
+        ofd2pdf.toPdf(ofd1.getDocuments().get(0), new File(basePath,"旋转测试.pdf"));
     }
 }
