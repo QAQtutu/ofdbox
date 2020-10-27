ac2c2d195d4ac947a69db05f1373b6a9c60bb57c
zhangjiayao <203524203@qq.com>
2020-10-27 14:16:47 +0800

    fix:修复转PDF图片旋转问题

--- ordbox-converter/src/main/java/com/qaqtutu/ofdbox/convertor/pdf/Ofd2pdf.java
+++ ordbox-converter/src/main/java/com/qaqtutu/ofdbox/convertor/pdf/Ofd2pdf.java
@@ -47,41 +47,42 @@ public class Ofd2pdf {
     private Config config = new Config();
 
     //1英寸=72pt
-    private static final  int in2pt=72;
+    private static final int in2pt = 72;
     //1英寸=25.4毫米
-    private static final float in2mm=25.4f;
+    private static final float in2mm = 25.4f;
     //1毫米=1/25.4英寸
-    private static final float mm2in=1.0f/in2mm;
+    private static final float mm2in = 1.0f / in2mm;
     //1毫米=72/25.4英寸
-    private static final float mm2pt=in2pt/in2mm;
+    private static final float mm2pt = in2pt / in2mm;
 
     private PDFont font;
 
 
-    public void toPdf(Document doc,File file){
-        try(OutputStream out=new FileOutputStream(file)) {
-            toPdf( doc, out);
+    public void toPdf(Document doc, File file) {
+        try (OutputStream out = new FileOutputStream(file)) {
+            toPdf(doc, out);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-    public void toPdf(Document doc,OutputStream out){
+
+    public void toPdf(Document doc, OutputStream out) {
 
         PDDocument pdfDoc = null;
         try {
             pdfDoc = new PDDocument();
-             font= PDType0Font.load(pdfDoc, new File("C:\\Windows\\Fonts\\Dengl.ttf"));
-            for(Page page:doc.getPages()){
+            font = PDType0Font.load(pdfDoc, new File("C:\\Windows\\Fonts\\Dengl.ttf"));
+            for (Page page : doc.getPages()) {
 
                 ST_Box pageBox = page.getPhysicalBox();
                 System.out.println(pageBox);
-                PDPage pdfPage = new PDPage(new PDRectangle(pageBox.getX().floatValue()*mm2pt, pageBox.getY().floatValue()*mm2pt, pageBox.getW().floatValue()*mm2pt, pageBox.getH().floatValue()*mm2pt));
+                PDPage pdfPage = new PDPage(new PDRectangle(pageBox.getX().floatValue() * mm2pt, pageBox.getY().floatValue() * mm2pt, pageBox.getW().floatValue() * mm2pt, pageBox.getH().floatValue() * mm2pt));
 
 
                 PDPageContentStream contStr = new PDPageContentStream(pdfDoc, pdfPage);
 
-                renderPage(pdfDoc,contStr, page,pageBox);
+                renderPage(pdfDoc, contStr, page, pageBox);
 
                 contStr.close();
 
@@ -99,7 +100,7 @@ public class Ofd2pdf {
     }
 
 
-    private void renderPage(PDDocument document,PDPageContentStream contStr,Page page,ST_Box pageBox) {
+    private void renderPage(PDDocument document, PDPageContentStream contStr, Page page, ST_Box pageBox) {
         List<NLayer> bodyLayer = new ArrayList<>();
         List<NLayer> foreLayer = new ArrayList<>();
         List<NLayer> backLayer = new ArrayList<>();
@@ -133,40 +134,40 @@ public class Ofd2pdf {
         }
         //背景层模版
         for (Template template : backTemplate)
-            renderPage(document,contStr,template,pageBox);
+            renderPage(document, contStr, template, pageBox);
         //背景层
         for (NLayer layer : backLayer)
-            renderContent(document,contStr, page,layer,pageBox);
+            renderContent(document, contStr, page, layer, pageBox);
         //正文层模版
         for (Template template : bodyTemplate)
-            renderPage(document,contStr,template,pageBox);
+            renderPage(document, contStr, template, pageBox);
         //正文层
         for (NLayer layer : bodyLayer)
-            renderContent(document,contStr, page,layer,pageBox);
+            renderContent(document, contStr, page, layer, pageBox);
         //前景层模版
         for (Template template : foreTemplate)
-            renderPage(document,contStr,template,pageBox);
+            renderPage(document, contStr, template, pageBox);
         //前景层
         for (NLayer layer : foreLayer)
-            renderContent(document,contStr, page,layer,pageBox);
+            renderContent(document, contStr, page, layer, pageBox);
 
     }
 
-    private void renderContent(PDDocument document,PDPageContentStream contStr,Page page, CT_PageBlock pageBlock,ST_Box pageBox) {
+    private void renderContent(PDDocument document, PDPageContentStream contStr, Page page, CT_PageBlock pageBlock, ST_Box pageBox) {
 
         new ContentWalker(pageBlock) {
             @Override
             public void onText(NTextObject nTextObject) {
                 try {
 
-                    for(NTextCode nTextCode:nTextObject.getTextCodes()){
+                    for (NTextCode nTextCode : nTextObject.getTextCodes()) {
 
 
                         Double[] deltaX = formatDelta(nTextCode.getContent().length(), nTextCode.getDeltaX());
                         Double[] deltaY = formatDelta(nTextCode.getContent().length(), nTextCode.getDeltaY());
                         double dx = nTextCode.getX().floatValue();
                         double dy = nTextCode.getY().floatValue();
-                        for(int i=0;i<nTextCode.getContent().length();i++){
+                        for (int i = 0; i < nTextCode.getContent().length(); i++) {
                             //不是第一个字，就加上偏移量
                             if (i != 0) {
                                 dx += deltaX[i - 1].floatValue();
@@ -174,29 +175,29 @@ public class Ofd2pdf {
                             }
 
 
-                            String s=String.valueOf(nTextCode.getContent().charAt(i));
+                            String s = String.valueOf(nTextCode.getContent().charAt(i));
 
-                            Matrix matrix= MatrixUtils.base();
-                            matrix=MatrixUtils.imageMatrix(matrix,0,1,0);
-                            if(nTextObject.getCtm()!=null){
-                                Matrix ctm=MatrixUtils.ctm(nTextObject.getCtm());
-                                matrix=matrix.mtimes(ctm);
+                            Matrix matrix = MatrixUtils.base();
+                            matrix = MatrixUtils.imageMatrix(matrix, 0, 1, 0);
+                            if (nTextObject.getCtm() != null) {
+                                Matrix ctm = MatrixUtils.ctm(nTextObject.getCtm());
+                                matrix = matrix.mtimes(ctm);
 
                             }
                             ST_Box boundary = nTextObject.getBoundary();
-                            if(boundary==null){
-                                boundary=pageBox;
+                            if (boundary == null) {
+                                boundary = pageBox;
                             }
-                            matrix=matrix.mtimes(MatrixUtils.create(1,0,0,1,dx,dy));
-                            matrix=matrix.mtimes(MatrixUtils.create(1,0,0,1,boundary.getX(),boundary.getY()));
-                            matrix=MatrixUtils.imageMatrix(matrix,0,1,0);
-                            matrix=matrix.mtimes(MatrixUtils.create(1,0,0,1,0,pageBox.getH()));
-                            matrix=matrix.mtimes(MatrixUtils.create(mm2pt,0,0,mm2pt,0,0));
+                            matrix = matrix.mtimes(MatrixUtils.create(1, 0, 0, 1, dx, dy));
+                            matrix = matrix.mtimes(MatrixUtils.create(1, 0, 0, 1, boundary.getX(), boundary.getY()));
+                            matrix = MatrixUtils.imageMatrix(matrix, 0, 1, 0);
+                            matrix = matrix.mtimes(MatrixUtils.create(1, 0, 0, 1, 0, pageBox.getH()));
+                            matrix = matrix.mtimes(MatrixUtils.create(mm2pt, 0, 0, mm2pt, 0, 0));
 
 //                            contStr.transform(toPFMatrix(matrix));
 
 
-                            contStr.setFont(font,Double.valueOf(nTextObject.getSize()).floatValue());
+                            contStr.setFont(font, Double.valueOf(nTextObject.getSize()).floatValue());
                             contStr.beginText();
                             /*
                              * 文字默认填充 不勾边
@@ -237,46 +238,43 @@ public class Ofd2pdf {
             @Override
             public void onImage(NImageObject nImageObject) {
                 OFDFile ofdFile = page.getDocument().getMultiMedia(nImageObject.getResourceId().getId());
-                if(ofdFile==null){
-                    log.warn("找不到图片："+ofdFile.getLoc());
-                };
+                if (ofdFile == null) {
+                    log.warn("找不到图片：" + ofdFile.getLoc());
+                }
+                ;
                 try {
 
                     Tuple3<byte[], Integer, Integer> image = ImageUtils.imageConvert(ofdFile);
                     PDImageXObject pdImage = PDImageXObject.createFromByteArray(document, image.getFirst(), ofdFile.getLoc());
 
-                    Matrix matrix= MatrixUtils.base();
-                    matrix=MatrixUtils.imageMatrix(matrix,0,1,0);
-                    if(nImageObject.getCtm()!=null){
-                        Matrix ctm= MatrixUtils.base();
-                        ctm=ctm.mtimes(MatrixUtils.ctm(nImageObject.getCtm()));
-                        Tuple2<Double,Double> tuple2=MatrixUtils.leftTop(ctm);
-                        ctm = ctm.mtimes(MatrixUtils.create(1, 0, 0, 1, tuple2.getFirst().floatValue(),tuple2.getSecond().floatValue()));
-
-                        matrix=matrix.mtimes(ctm);
-
+                    Matrix matrix = MatrixUtils.base();
+                    matrix = MatrixUtils.imageMatrix(matrix, 0, 1, 0);
+                    matrix = MatrixUtils.move(matrix, 0, 1);
+                    if (nImageObject.getCtm() != null) {
+                        Matrix ctm = MatrixUtils.base();
+                        ctm = ctm.mtimes(MatrixUtils.ctm(nImageObject.getCtm()));
+                        matrix = matrix.mtimes(ctm);
                     }
-
 
                     ST_Box boundary = nImageObject.getBoundary();
-                    if(boundary==null){
-                        boundary=pageBox;
+                    if (boundary == null) {
+                        boundary = pageBox;
                     }
 
-                    matrix=matrix.mtimes(MatrixUtils.create(1,0,0,1,boundary.getX(),boundary.getY()));
-                    matrix=matrix.mtimes(MatrixUtils.create(1,0,0,1,0,-boundary.getH()));
+                    Tuple2<Double, Double> tuple2 = MatrixUtils.leftTop(matrix);
+//                    matrix = matrix.mtimes(MatrixUtils.create(1, 0, 0, 1, tuple2.getFirst().floatValue(),tuple2.getSecond().floatValue()));
 
-                    matrix=MatrixUtils.imageMatrix(matrix,0,1,0);
-                    matrix=matrix.mtimes(MatrixUtils.create(1,0,0,1,0,pageBox.getH()));
+                    matrix = matrix.mtimes(MatrixUtils.create(1, 0, 0, 1, boundary.getX(), boundary.getY()));
 
-                    matrix=matrix.mtimes(MatrixUtils.create(mm2pt,0,0,mm2pt,0,0));
+//                    matrix=matrix.mtimes(MatrixUtils.create(1,0,0,1,0,boundary.getH()));
+
+                    matrix = MatrixUtils.imageMatrix(matrix, 0, 1, 0);
+                    matrix = matrix.mtimes(MatrixUtils.create(1, 0, 0, 1, 0, pageBox.getH()));
+
+                    matrix = matrix.mtimes(MatrixUtils.create(mm2pt, 0, 0, mm2pt, 0, 0));
 
 
-
-
-
-
-                    contStr.drawImage(pdImage,toPFMatrix(matrix));
+                    contStr.drawImage(pdImage, toPFMatrix(matrix));
 //                    contStr.drawImage(pdImage, 0, -50, 1, 1);
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -290,14 +288,14 @@ public class Ofd2pdf {
         }.walk();
     }
 
-    private static org.apache.pdfbox.util.Matrix toPFMatrix(Matrix source){
-        org.apache.pdfbox.util.Matrix target=new org.apache.pdfbox.util.Matrix();
-        target.setValue(0,0,source.getAsFloat(0,0));
-        target.setValue(0,1,source.getAsFloat(0,1));
-        target.setValue(1,0,source.getAsFloat(1,0));
-        target.setValue(1,1,source.getAsFloat(1,1));
-        target.setValue(2,0,source.getAsFloat(2,0));
-        target.setValue(2,1,source.getAsFloat(2,1));
+    private static org.apache.pdfbox.util.Matrix toPFMatrix(Matrix source) {
+        org.apache.pdfbox.util.Matrix target = new org.apache.pdfbox.util.Matrix();
+        target.setValue(0, 0, source.getAsFloat(0, 0));
+        target.setValue(0, 1, source.getAsFloat(0, 1));
+        target.setValue(1, 0, source.getAsFloat(1, 0));
+        target.setValue(1, 1, source.getAsFloat(1, 1));
+        target.setValue(2, 0, source.getAsFloat(2, 0));
+        target.setValue(2, 1, source.getAsFloat(2, 1));
         return target;
     }
 
@@ -313,12 +311,12 @@ public class Ofd2pdf {
         String[] s = deltaStr.split("\\s+");
         int i = 0;
         int counter = 0;
-        while (i < s.length && counter<textLength-1) {
+        while (i < s.length && counter < textLength - 1) {
             String current = s[i];
             if ("g".equals(current)) {
                 Integer num = Integer.valueOf(s[i + 1]);
                 Double delta = Double.valueOf(s[i + 2]);
-                for (int j = 1; j <= num && counter<textLength-1; j++) {
+                for (int j = 1; j <= num && counter < textLength - 1; j++) {
                     arr[counter] = delta;
                     counter++;
                 }
