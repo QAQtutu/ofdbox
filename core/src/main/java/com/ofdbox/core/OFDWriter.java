1f7a83e379e07fe00c490278d3de3e8e038c9365
qaqtutu <203524203@qq.com>
2020-11-05 23:32:09 +0800

    chore:添加源码打包插件，同时将zip解压从ant切换到jdk自带工具

--- core/src/main/java/com/ofdbox/core/OFDWriter.java
+++ core/src/main/java/com/ofdbox/core/OFDWriter.java
@@ -1,22 +1,21 @@
 package com.ofdbox.core;
 
-import org.apache.tools.zip.ZipEntry;
-import org.apache.tools.zip.ZipOutputStream;
-
 import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class OFDWriter {
 
-    public static void write(OFD ofd,File file){
+    public static void write(OFD ofd, File file) {
         System.out.println(file);
 
         try {
             OutputStream out = new FileOutputStream(file);
             BufferedOutputStream bos = new BufferedOutputStream(out);
             ZipOutputStream zipOutputStream = new ZipOutputStream(bos);
-            ofd.getFileManager().files().forEach(f->{
-                String path=f.substring(f.indexOf("/")+1);
-                ZipEntry zipEntry=new ZipEntry(path);
+            ofd.getFileManager().files().forEach(f -> {
+                String path = f.substring(f.indexOf("/") + 1);
+                ZipEntry zipEntry = new ZipEntry(path);
 
 //                System.out.println(path);
                 try {
@@ -36,7 +35,7 @@ public class OFDWriter {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ofd.getFileManager().files().forEach(f->{
+        ofd.getFileManager().files().forEach(f -> {
 //            System.out.println(f);
         });
     }
