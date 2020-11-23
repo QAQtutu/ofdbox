435fbe60c7e0dd71d166322200afcad9cadf67a4
zhangjiayao <203524203@qq.com>
2020-11-23 17:49:48 +0800

    去除敏感代码

--- converter/src/test/java/com/ofdbox/convertor/test/img/ImgConverterTest.java
+++ converter/src/test/java/com/ofdbox/convertor/test/img/ImgConverterTest.java
@@ -21,39 +21,15 @@ public class ImgConverterTest {
 
         OFDReader reader = new OFDReader();
         reader.getConfig().setValid(false);
-        OFD ofd = reader.read(new File(basePath + "发票.ofd"));
+
         OFD ofd1 = reader.read(new File(basePath + "旋转测试.ofd"));
 
         Ofd2Img ofd2Img = new Ofd2Img();
         ofd2Img.getConfig().setDrawBoundary(false);
 
-        BufferedImage image = ofd2Img.toImage(ofd.getDocuments().get(0).getPages().get(0), 20);
         BufferedImage image1 = ofd2Img.toImage(ofd1.getDocuments().get(0).getPages().get(0), 20);
 
-        ImageIO.write(image, "JPEG", new FileOutputStream(new File(basePath, "发票.jpg")));
         ImageIO.write(image1, "JPEG", new FileOutputStream(new File(basePath, "旋转测试.jpg")));
 
-    }
-
-    @Test
-    public void toImgWithSeal() throws Exception {
-        final int dpi = 20;
-        // 生成印章
-        String mainText = "全国统一发票监制章";
-        String viceText = "宁波市税务局";
-        String centerText = "国家税务总局";
-        BufferedImage seal = SealUtil.buildInvoiceSeal(dpi, mainText, viceText, centerText);
-
-        OFDReader reader = new OFDReader();
-        reader.getConfig().setValid(false);
-        OFD ofd = reader.read(new File(basePath + "发票.ofd"));
-
-
-        Ofd2Img ofd2Img = new Ofd2Img();
-        ofd2Img.getConfig().setDrawBoundary(false);
-
-        BufferedImage image = ofd2Img.toImage(ofd.getDocuments().get(0).getPages().get(0), 20);
-        SealUtil.drawInto(seal,image);
-        ImageIO.write(image, "JPEG", new FileOutputStream(new File(basePath, "0000000005.jpg")));
     }
 }
